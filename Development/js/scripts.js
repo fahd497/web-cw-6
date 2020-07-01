ea6148ec83923434e4aa5a349a9a180e666c7d37
fahdhadhoud <66736069+fahdhadhoud@users.noreply.github.com>
2020-07-01 23:26:24 +0300

    ok1

---
+++ Development/js/scripts.js
@@ -0,0 +1,53 @@
+function logger(name) {
+ for(let fahd of name ) 
+{
+
+    console.log(name)
+}
+
+}
+let fahd =["hussain","moudy","nancy"]
+
+logger(fahd)
+
+
+
+let play=["10","15","20","25"]
+
+function  hottestDays(y,k){
+for(let f of y ){
+    if(f > k)
+    console.log(f)
+}
+
+}
+
+hottestDays(play , 16);
+
+let no=[
+
+{
+bookk:"read" ,id:1},
+
+{bookk:"play" ,id:2},
+    
+{bookk:"how" ,id:3},
+    
+];
+getBookById(2,no);
+function getBookById(bookId, books){
+
+for(book of no ){
+if(book.id==bookId)
+console.lpg(book.bookk);
+
+
+
+  else {
+    console.log("undefined");
+ 
+}
+
+}
+
+
